b0df312258e2e3ea8c453d11e590cd898eb46ba3
Jan Henrik Uemann <jan-henrik.uemann@arvato-scs.com>
2022-08-23 14:58:17 +0200

    changed color

--- src/logic.ts
+++ src/logic.ts
@@ -5,7 +5,7 @@ export function info(): InfoResponse {
     const response: InfoResponse = {
         apiversion: "1",
         author: "",
-        color: "#888888",
+        color: "#ff19e0",
         head: "default",
         tail: "default"
     }
