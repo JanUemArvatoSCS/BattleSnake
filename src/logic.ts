dde5e65337c6fecc340608eecfe6d734c6ff2402
Jan Henrik Uemann <jan-henrik.uemann@arvato-scs.com>
2022-08-29 11:47:38 +0200

    buggs fixed

--- src/logic.ts
+++ src/logic.ts
@@ -39,7 +39,7 @@ export function move(gameState: GameState): MoveResponse {
 
     printPlayBoard(playBoard);
 
-    var counter:number = 10;
+    var counter:number = 3;
     while(counter > 0){
         playBoard = prepareArrayForFlatting(playBoard);
         counter--;
