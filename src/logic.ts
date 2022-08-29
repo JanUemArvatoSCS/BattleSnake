f87e719498b418e155e0ff5cb0f4d3575f4f2786
Jan Henrik Uemann <jan-henrik.uemann@arvato-scs.com>
2022-08-29 12:00:24 +0200

    test

--- src/logic.ts
+++ src/logic.ts
@@ -39,7 +39,7 @@ export function move(gameState: GameState): MoveResponse {
 
     printPlayBoard(playBoard);
 
-    var counter:number = 3;
+    var counter:number = 6;
     while(counter > 0){
         playBoard = prepareArrayForFlatting(playBoard);
         counter--;
